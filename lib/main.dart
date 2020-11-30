e279d6635db35a8b1f0dea32a8677aa4817a654d
Raymond <raymond96521>
2020-11-30 18:30:00 +0800

    Add widgets into the column

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,37 @@ import 'package:flutter/material.dart';
 // void main() {
 //   runApp(MyApp());
 // }
-void main => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s is your favourite color?',
+      'What\'s is your favourite animal?'
+    ];
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
             title: Text('My First App'),
           ),
-          body: Text('This is my default text')),
+          body: Column(
+            children: [
+              Text('The question!'),
+              RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: null,
+              ),
+              RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: null,
+              ),
+            ],
+          )),
     );
   }
 }
